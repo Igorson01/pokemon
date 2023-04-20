5221c89cbeb2f9fc4012cca54ad7c0c1a27da5e3
Igorson01 <josefkrolewski@gmail.com>
2023-04-20 13:24:44 +0200

    all

---
+++ data/audio.js
@@ -0,0 +1,37 @@
+const audio = {
+    Map: new Howl({
+        src: './audio/map.wav',
+        html5: true,
+        volume: 0.02
+    }),
+    initBattle: new Howl({
+        src: './audio/initBattle.wav',
+        html5: true,
+        volume: 0.02
+    }),
+    battle: new Howl({
+        src: './audio/battle.mp3',
+        html5: true,
+        volume: 0.02
+    }),
+    fireBallHit: new Howl({
+        src: './audio/fireBallHit.wav',
+        html5: true,
+        volume: 0.02
+    }),
+    initFireball: new Howl({
+        src: './audio/initFireball.wav',
+        html5: true,
+        volume: 0.02
+    }),
+    tackleHit: new Howl({
+        src: './audio/tackleHit.wav',
+        html5: true,
+        volume: 0.02
+    }),
+    victory: new Howl({
+        src: './audio/victory.wav',
+        html5: true,
+        volume: 0.05
+    }),
+}
